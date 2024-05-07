508fa93c712d077834545aaabb2e6dfe8bcfc502
samuelisboa <162823121+samuelisboa@users.noreply.github.com>
2024-05-07 09:23:08 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+$('.slide-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+});
